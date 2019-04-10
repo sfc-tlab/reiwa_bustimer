1325a7bde1754959ecab36b2b79cc27a2458faa5
im-neko <lukepuyuki0430@gmail.com>
2019-04-10 20:37:36 +0900

    [WIP] styled-componentsの導入

--- components/TweetButton.tsx
+++ components/TweetButton.tsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import styled, { css } from 'styled-components';
 
+
+const styles = {
+  logo: {
+    margin: 0,
+    color: '#1b95e0',
+  }
+};
 
 class TweetButton extends Component {    
 
-  componentWillMount () {
+  private componentWillMount () {
     this.setState({ ...this.props });
   }
 
-  componentWillReceiveProps(nextProps) {
+  private componentWillReceiveProps(nextProps) {
     this.setState({ ...nextProps });
   } 
 
-  postTweet = () => {
+  private postTweet = () => {
     let url = "https://twitter.com/share?";
     const params = {
       "text": this.state.text,
@@ -23,17 +31,34 @@ class TweetButton extends Component {
     window.open(url);
   }
 
-  render () {
+  public render () {
     const { children } = this.state;
 
     return (
-      <span className="tweet-button"
+      <Button
         onClick={this.postTweet} 
       >
         {children}
-      </span>
+        <Icon 
+          src="/static/img/twitter-icon.svg" 
+        />
+      </Button>
     )
   }
 }
+
+const Button = styled.button`
+  border-radius: 3px;
+  border: 2px solid #1b95e0;
+  color: #1b95e0;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
+
+const Icon = styled.img`
+  coler: #1b95e0;
+  size: 100px;
+`;
+
 
 export default TweetButton;
