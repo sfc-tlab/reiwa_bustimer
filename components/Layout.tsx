914eae5a8cc903029535fea3c724a8019e9b969e
im-neko <lukepuyuki0430@gmail.com>
2019-05-12 23:15:21 +0900

    ogpのメッセージを仮置き

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -22,18 +22,19 @@ class Layout extends Component {
           <title>reiwa no bustimer</title>
           <meta name="format-detection" content="telephome=no" />
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta name="keywords" content="bustimer,SFC,sfc,バス" />
           <meta name="description" content={store.ogpDescription} />
           <meta property="og:title" content="reiwa no bustimer - SFC生のバス通学サポートアプリ" />
           <meta property="og:description" content={store.ogpDescription} />
-          <meta name="keywords" content="bustimer,SFC,sfc,バス" />
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://bustimer.sfc.keioac.jp" />
-
           <meta property="og:image" content="https://bustimer.sfc.keioac.jp/static/icon.png" />
           <meta property="og:image:width" content="600" />
           <meta property="og:image:height" content="300" />
           <meta property="og:site_name" content="reiwa no bustimer" />
+
           <meta property="fb:app_id" content="" />
+
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:url" content="https://bustimer.sfc.keioac.jp" />
           <meta name="twitter:title" content="reiwa no bustimer" />
