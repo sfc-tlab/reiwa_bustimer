7ee39606388c07d6e55e5ac51199366cac134493
im-neko <yuki@ideta.net>
2019-12-31 00:02:55 +0900

    バナー薄すぎた...

--- front/components/Banner.tsx
+++ front/components/Banner.tsx
@@ -49,7 +49,7 @@ const Text = styled.p`
 const Wrapper = styled.div`
   display: ${props => props.isClose? 'none' : ''};
   position: fixed;
-  background-color: rgba(255, 241, 240, 0.5);
+  background-color: rgba(255, 241, 240, 0.8);
   width: 100%;
   bottom: 0px;
 `;
